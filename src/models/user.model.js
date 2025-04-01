dabf8fec4a7541033299a06da071c426c1d3ee47
rameshneel <ramesh@neelnewtworks.com>
2025-04-01 16:23:36 +0530

    Fix import statement in user model to include file extension for consistency

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,7 +1,7 @@
 import mongoose, { Schema } from "mongoose";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
-import Customer from "./customer.model";
+import Customer from "./customer.model.js";
 
 const userSchema = new Schema(
   {
